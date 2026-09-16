9d992a48fc3b6c3c0423dd057b7e6033fe8b2733
Varendra Gm <varendragm@gmail.com>
2026-09-16 07:53:15 +0700

    Add role authorization middleware

---
+++ app/Http/Middleware/EnsureRole.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureRole
+{
+    public function handle(Request $request, Closure $next, string ...$roles): Response
+    {
+        $user = $request->user();
+
+        if (!$user || !in_array($user->role, $roles, true)) {
+            return response()->json([
+                'message' => 'Anda tidak memiliki akses untuk melakukan tindakan ini.',
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
