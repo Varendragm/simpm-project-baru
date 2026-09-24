a91c848537f0b56404323eaa2866bcea8fa432f2
Varendra Gm <varendragm@gmail.com>
2026-09-24 09:17:28 +0700

    fix: enforce PM assignment on create and report routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\StationController;
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\EnsurePmScheduleAssignment;
 use App\Http\Middleware\EnsureRole;
 use Illuminate\Support\Facades\Route;
 
@@ -28,12 +29,14 @@ Route::middleware('auth')->group(function () {
             Route::post('/machines', [MachineController::class, 'store']);
             Route::put('/machines/{machine}', [MachineController::class, 'update']);
             Route::delete('/machines/{machine}', [MachineController::class, 'destroy']);
-            Route::post('/pm-schedules', [PmScheduleController::class, 'store']);
+            Route::post('/pm-schedules', [PmScheduleController::class, 'store'])
+                ->middleware(EnsurePmScheduleAssignment::class);
             Route::post('/pm-schedules/{pmSchedule}/validasi', [PmScheduleController::class, 'validasi']);
         });
 
         Route::middleware(EnsureRole::class . ':teknisi')->group(function () {
-            Route::post('/pm-schedules/{pmSchedule}/laporan', [PmScheduleController::class, 'submitLaporan']);
+            Route::post('/pm-schedules/{pmSchedule}/laporan', [PmScheduleController::class, 'submitLaporan'])
+                ->middleware(EnsurePmScheduleAssignment::class);
         });
 
         Route::post('/profile/password', [ProfileController::class, 'updatePassword']);
